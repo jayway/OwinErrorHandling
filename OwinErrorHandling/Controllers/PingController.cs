f135b74ce9573d58be111efc8a754855eb7e7ec6
Tomas Lycken <tomas.lycken@gmail.com>
2016-01-13 17:34:13 +0100

    Baseline
    Go to / for a working url, or /oops to see an exception thrown inside
    Web API.

---
+++ OwinErrorHandling/Controllers/PingController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Web.Http;
+
+namespace OwinErrorHandling.Controllers
+{
+    public class PingController : ApiController
+    {
+        [Route(""), HttpGet]
+        public IHttpActionResult Ping()
+        {
+            return Ok(DateTime.UtcNow);
+        }
+
+        [Route("oops"), HttpGet]
+        public IHttpActionResult Oops()
+        {
+            throw new Exception("oops!");
+        }
+    }
+}
---
+++ OwinErrorHandling/Startup.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Net.Http.Formatting;
+using System.Web.Http;
+using Microsoft.Owin;
+using Owin;
+using OwinErrorHandling;
+
+[assembly: OwinStartup(typeof(Startup))]
+
+namespace OwinErrorHandling
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            var config = new HttpConfiguration();
+            config.MapHttpAttributeRoutes();
+            config.Formatters.Clear();
+            config.Formatters.Add(new JsonMediaTypeFormatter());
+
+            app
+                .UseWebApi(config);
+        }
+    }
+}
